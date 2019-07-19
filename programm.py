d3afdb5d109dc0f79e1431aca55a42a5a6131b4f
Ivan Skotar <ivan@dokkio.com>
2019-07-19 14:32:31 -0700

    Menu option

---
+++ programm.py
@@ -0,0 +1,14 @@
+def print_menu():
+    print('\nMain Menu\n'
+          '---------\n'
+          '0 - Convert temperature\n'
+          '1 - Process a new data file\n'
+          '2 - Choose units\n'
+          '3 - Edit room filter\n'
+          '4 - Show summary statistics\n'
+          '5 - Show temperature by date and time\n'
+          '6 - Show histogram of temperatures\n'
+          '7 - Quit\n'
+          '')
+
+print_menu()
