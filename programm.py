ee109b02fe830324b8c2426f37c617d4a850e632
Ivan Skotar <ivan@dokkio.com>
2019-09-15 21:42:13 -0700

    - convert units - new file - UNITS - chose units - updates

--- programm.py
+++ programm.py
@@ -1,4 +1,18 @@
+from func import TempDataset
+
 from collections import OrderedDict
+
+current_set = TempDataset()
+current_set.data_set = []
+curr_unit = 0
+
+def convert_units(celsius_value, units):
+    if units == 0:
+        return celsius_value
+    if units == 1:
+        return celsius_value * 1.8 + 32
+    if units == 2:
+        return celsius_value + 273.15
 
 def print_menu():
     print('\nMain Menu\n'
@@ -13,16 +27,6 @@ def print_menu():
           '7 - Quit\n'
           '')
 
-print_menu()
-
-def convert_units(celsius_value, units):
-    if units == 0:
-        return celsius_value
-    if units == 1:
-        return celsius_value * 1.8 + 32
-    if units == 2:
-        return celsius_value + 273.15
-
 sensors = {
     "4213": ("STEM Center", 0),
     "4201": ("Foundations Lab", 1),
@@ -32,43 +36,45 @@ sensors = {
     "Out": ("Outside", 5),
 }
 
-sortedSensors = OrderedDict(sorted(sensors.items(), key=lambda x: x[0]))
+sorted_sensors = OrderedDict(sorted(sensors.items(), key = lambda x: x[0]))
 
 filter_list = []
 
+for a, b in sensors.items():
+    filter_list.append(b[1])
 
 def print_filter(filter_list):
     print('\nEdit room filter\n'
           '----------------')
-    for a, b in sortedSensors.items():
+    for a, b in sorted_sensors.items():
         if b[1] in filter_list:
-            print(a + ":", b[0])
+            print(a + ":", b[0], '[ACTIVE]')
         else:
-            print(a + ':', b[0], '[ACTIVE]')
+            print(a + ':', b[0])
     print('\nType "x" to return to Main Menu \n'
           'OR')
 
-def change_filter(sortedSensors, filter_list):
+def change_filter(sorted_sensors, filter_list):
     print_filter(filter_list)
-
-    option = input('Type the room number to deactivate sensor: ')
+    option = input('Type the room number (e.g.4201) to deactivate sensor: ')
     if option == 'x':
         programm()
-    elif option in sortedSensors:
-        for a, b in sortedSensors.items():
+    elif option in sorted_sensors:
+        for a, b in sorted_sensors.items():
             if option == a:
                 if b[1] in filter_list:
                     filter_list.remove(b[1])
                 else:
                     filter_list.append(b[1])
-        change_filter(sortedSensors, filter_list)
+        change_filter(sorted_sensors, filter_list)
     else:
         print('Invalid Sensor\n')
-        change_filter(sortedSensors, filter_list)
+        change_filter(sorted_sensors, filter_list)
 
 def programm():
     print_menu()
-    option = input('What is your choice? ')
+    print(current_set.get_avg_temperature_day_time(filter_list, 5, 7))
+    option = input('What is your choice?: ')
     try:
         option = int(option)
         if option < 0 or option > 7:
@@ -83,13 +89,72 @@ def programm():
                 kelvin_t = convert_units(float(celsius_t), 2)
                 print('\nThat\'s', fahrenheit_t, '°F and', kelvin_t, '°K')
                 programm()
+            elif option == 1:
+                new_file()
+                programm()
+            elif option == 2:
+                choose_units()
+                programm()
             elif option == 3:
-                change_filter(sortedSensors, filter_list)
+                change_filter(sorted_sensors, filter_list)
+            elif option == 4:
+                try:
+                    print(current_set.get_summary_statistics(filter_list), UNITS[curr_unit])
+                except:
+                    print('\nPlease load data file and make sure at least one sensor is active')
+                    programm()
+            elif option == 5:
+                # day = int(input('Enter a week day in numbers (e.g 0-6): '))
+                # time = float(input('Enter a Time in 24h format: '))
+                # print(current_set.get_avg_temperature_day_time(filter_list, day, time))
+                programm()
             else:
                 print('\nOption 1 selected\n')
                 programm()
     except:
         print('\n*** Please enter an integer only ***\n')
         programm()
+
+print('STEM Center Temperature Project\n'
+      'Iullia Skotar\n'
+      '')
+
+def new_file():
+    filename = input('Enter the file name of the new dataset: ')
+
+    if current_set.process_file(filename):
+        print('Loaded', len(current_set.data_set), 'number of samples')
+
+        dataname = input('Please Enter 3 to 20 characters for the data name: ')
+        while current_set.set_name(dataname) is False:
+            print('Wrong name!')
+            dataname = input('Please Enter 3 to 20 characters for the data name: ')
+    else:
+        print('File is not exist or incorrect format')
+
+UNITS = {
+    0: ("Celsius", "C"),
+    1: ("Fahrenheit", "F"),
+    2: ("Kelvin", "K"),
+    }
+
+def choose_units():
+    global curr_unit
+    print('\nCurrent unit is:', UNITS[curr_unit][0])
+    print('\nChoose new units\n'
+          '----------------')
+    for key, value in UNITS.items():
+        print(key, '-', value[0])
+    unit_selection = input('\nWhich unit?: ')
+    try:
+        unit_selection = int(unit_selection)
+        if unit_selection in UNITS.keys():
+            curr_unit = unit_selection
+        else:
+            print('Please choose a unit from the list')
+            choose_units()
+    except:
+        print('*** Please enter a number only ***')
+        choose_units()
 
 programm()
